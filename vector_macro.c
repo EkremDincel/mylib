cc4b8af448a4a24833cb9db51a73a247764a05b2
Ekrem Dinçel <ekremdincel@gmail.com>
2020-09-11 18:50:35 +0300

    Added vector macros.

--- vector_macro.c
+++ vector_macro.c
@@ -5,39 +5,51 @@ static const int size_multiplier = 2;
 
 #define __vector_size_type int
 
-#define vector_create(v) 																								  \
-do 																														  \
-{	/*                     ||       vector lenght 	   | 		vector capacity 	| 	vector data 		||			*/\
-	/*                                                                              ^									*/\
-	/*                                                                             [v] pointer							*/\
-	char *vector = mymalloc(sizeof(__vector_size_type) + sizeof(__vector_size_type) + (sizeof(v) * init_size));			  \
-	printf("first head %i\n", vector);\
-	v = (void *)(vector + sizeof(__vector_size_type) * 2);																  \
-	_vector_set_length(v, 0);																							  \
-	_vector_set_capacity(v, init_size);																					  \
-} while (0)
+#define vector_init(v) vector_from_capacity((v), init_size)
 
-#define vector_length(v)			 (*(__vector_size_type *)(v - sizeof(__vector_size_type) * 2))
-#define _vector_set_length(v, n)     (*(__vector_size_type *)(v - sizeof(__vector_size_type) * 2) = n)
+#define vector_from_capacity(v, n) 																						  	  \
+	do 																														  \
+	{                                                                                                                         \
+		size_t *vector = mymalloc( sizeof(size_t) * 2 + (sizeof(*(v)) * (n)) );			                                      \
+		(v) = (void *)(&vector[2]);																                              \
+		vector_length(v) = 0;                                                                                                 \
+		vector_capacity(v) = (n);																				              \
+	} while (0)
 
-#define vector_capacity(v)			 (*(__vector_size_type *)(v - sizeof(__vector_size_type)))
-#define _vector_set_capacity(v, n)   (*(__vector_size_type *)(v - sizeof(__vector_size_type)) = n)
+#define vector_length(v)		(((size_t *)(v))[-2])
+#define vector_capacity(v)		(((size_t *)(v))[-1])
 
-#define vector_free(v)	\
-do 						\
-{						\
-	printf("%i, %i\n", v, (char *)v-8);\
-	free(((char *)v - 8));			\
-	printf("null\n");\
-	v = NULL;			\
+#define vector_append(v, value)	((v)[vector_length(v)++] = (value))
+#define vector_pop(v, value)	(vector_length(v)--)
+
+#define vector_last(v)			((v)[vector_length(v)-1])
+
+
+#define vector_iter(v, i) \
+	for (size_t __counter = 0, (i) = (v)[__counter]; __counter < vector_length(v); ++__counter, (i) = (v)[__counter])
+
+#define vector_enumerate(v, i, e) \
+	for (size_t __counter = 0, i=0, (e) = (v)[__counter]; __counter < vector_length(v); ++__counter, (i) = (v)[__counter], i=__counter)
+
+
+#define vector_free(v)					\
+do 										\
+{										\
+	free(&vector_length(v));			\
+	(v) = NULL;							\
 } while (0)
 
 
 int main(void) {
 	int *vec;
-	vector_create(vec);
-	printf("%i, %i\n", vec[-1], vec[-2]);
+	vector_init(vec);
+	vector_append(vec, 2);
+	vector_append(vec, 5);
 	printf("%i, %i\n", vector_length(vec), vector_capacity(vec));
+
+	int i, j;
+	vector_enumerate(vec, i, j) {
+		printf("%i:%i, ", i, j);
+	}
 	vector_free(vec);
-	printf("%s\n", "finish");
 }	
